13397764a668ca9b29b3da43bc9b02213b42f397
VasuGargDev <96354306+VasuGargDev@users.noreply.github.com>
2023-02-04 23:12:07 +0530

    1.3

---
+++ (1.3)index.js
@@ -0,0 +1,12 @@
+// Functions
+
+function add(numberOne, numberTwo) {
+  var sum = numberOne + numberTwo;
+  console.log("The sum of " +numberOne+ " and " +numberTwo+ " is:" );
+  return sum;
+}
+
+var result = add(2,5);
+console.log(result);
+
+console.log(add(10,10));
