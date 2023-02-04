d472bca1a296c4be82146ed95e6e3546b278e0ba
VasuGargDev <96354306+VasuGargDev@users.noreply.github.com>
2023-02-04 23:24:15 +0530

    1.7

---
+++ (1.7)index.js
@@ -0,0 +1,10 @@
+function log(data) {
+  console.log(data)
+}
+ 
+var groceryList = ["milk", "eggs", "flowers", "samosa", "pav"];
+
+
+for (var i=0; i<groceryList.length; i++) {
+  log(groceryList[i]);
+}
